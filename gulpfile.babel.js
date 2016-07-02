b005c61bbfa2588cd6243359a1199b66f0313c9d
Arjay Elbore <virtual@override-2.local>
2016-07-02 15:21:28 +0800

    add some files

---
+++ gulpfile.babel.js
@@ -0,0 +1,24 @@
+import gulp from 'gulp';
+import sourcemaps from 'gulp-sourcemaps';
+import concat from 'gulp-concat';
+import babel from 'gulp-babel';
+
+import { join } from 'path';
+import { readFileSync } from 'fs';
+
+let babelrc = JSON.parse(readFileSync(join(__dirname, '.babelrc'), 'utf-8'));
+let build_path = {
+    sources: [
+        'src/**/*.js',
+        '!src/test/**/*.js'
+    ]
+};
+
+gulp.task('build', () => {
+   return gulp.src(build_path.sources)
+        .pipe(sourcemaps.init())
+        .pipe(babel({ babelrc }))
+        .pipe(concat('all.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist')); 
+});
